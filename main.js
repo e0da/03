bbdb1e45527743c0b1b2e1c45fedab0ff8ea083a
Justin Force <justin.force@gmail.com>
2019-01-17 21:48:36 -0800

    Show logo

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
+const logoSrc = require("./logo.png");
 
 // SNES-ish resolution
 const width = 512;
@@ -9,16 +10,20 @@ canvas.width = width;
 canvas.height = height;
 
 // Draw a bordered rectangle with a greeting
-const borderWidth = 10;
-ctx.fillStyle = "#222";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.fillStyle = "#334";
-ctx.fillRect(
-  borderWidth,
-  borderWidth,
-  canvas.width - borderWidth * 2,
-  canvas.height - borderWidth * 2
-);
-ctx.fillStyle = "cyan";
-ctx.font = "50px sans-serif";
-ctx.fillText("hullo", 80, canvas.height / 2 + 15);
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+// Draw the logo
+const logo = new Image();
+logo.src = logoSrc;
+logo.onload = () => {
+  const scale = 5;
+  const w = logo.width * scale;
+  const h = logo.height * scale;
+  const x = width / 2 - w / 2;
+  const y = height / 2 - h / 2;
+  const originalSmoothing = ctx.imageSmoothingEnabled;
+  ctx.imageSmoothingEnabled = false;
+  ctx.drawImage(logo, x, y, w, h);
+  ctx.imageSmoothingEnabled = originalSmoothing;
+};
