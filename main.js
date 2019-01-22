efac6e4e8836bf3afcb9c316602abe88094b521a
Justin Force <justin.force@gmail.com>
2019-01-22 13:18:57 -0800

    Draw a bunch of stuff so I can debug frame rate

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ let state;
 // I picked this value because it's half of 400x300, and it gives nice chunky pixels
 const width = 192;
 const height = 108;
+const maxBalls = 100;
+const colors = ["white", "cyan", "magenta", "yellow"];
 
 const drawBackground = () => {
   const { canvas, ctx } = state;
@@ -15,6 +17,7 @@ const drawBackground = () => {
 
 const drawTexture = () => {
   const { ctx, texture } = state;
+  if (!texture.enabled) return;
   const scale = 1;
   const w = texture.img.width * scale;
   const h = texture.img.height * scale;
@@ -26,19 +29,20 @@ const drawTexture = () => {
   ctx.imageSmoothingEnabled = originalSmoothing;
 };
 
-const drawBall = () => {
-  const { ctx, ball } = state;
-  const colors = ["white", "cyan", "magenta", "yellow"];
-  const color = colors[Math.ceil(Math.random() * colors.length)];
-  ctx.fillStyle = color;
-  ctx.beginPath();
-  ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2);
-  ctx.fill();
+const drawBalls = () => {
+  const { ctx, balls } = state;
+  for (let i = 0; i < balls.length; i++) {
+    const ball = balls[i];
+    ctx.fillStyle = ball.color;
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.r, 0, Math.PI * 2);
+    ctx.fill();
+  }
 };
 
 const draw = () => {
   drawBackground();
-  drawBall();
+  drawBalls();
   drawTexture();
 };
 
@@ -57,17 +61,20 @@ const updateTexture = () => {
   else state.texture.img.height -= speed;
 };
 
-const updateBall = () => {
-  const { ball } = state;
-  if (ball.x > width || ball.x < 0) state.ball.vx *= -1;
-  if (ball.y > height || ball.y < 0) state.ball.vy *= -1;
-  state.ball.x += ball.vx;
-  state.ball.y += ball.vy;
+const updateBalls = () => {
+  const { balls } = state;
+  for (let i = 0; i < balls.length; i++) {
+    const ball = balls[i];
+    if (ball.x > width || ball.x < 0) ball.vx *= -1;
+    if (ball.y > height || ball.y < 0) ball.vy *= -1;
+    ball.x += ball.vx;
+    ball.y += ball.vy;
+  }
 };
 
 const update = () => {
   updateTexture();
-  updateBall();
+  updateBalls();
 };
 
 const step = () => {
@@ -83,26 +90,31 @@ const setupTexture = () => {
     img,
     xInhale: true,
     yInhale: true,
+    enabled: true,
     speed: 1
   };
 };
 
-const setupBall = () => {
-  const ballRadius = 3;
-  return {
-    x: width / 2 - ballRadius / 2,
-    y: height / 2 - ballRadius / 2,
-    r: ballRadius,
-    vx: 2,
-    vy: 1
-  };
+const setupBalls = () => {
+  let balls = [];
+  for (let i = 0; i < maxBalls; i++) {
+    const x = Math.floor(Math.random() * width);
+    const y = Math.floor(Math.random() * height);
+    const r = Math.random() * 3;
+    const vx = Math.random() * 3;
+    const vy = Math.random() * 3;
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    const ball = { x, y, r, vx, vy, color };
+    balls.push(ball);
+  }
+  return balls;
 };
 
 const setup = () => {
   const canvas = document.querySelector("canvas");
   const ctx = canvas.getContext("2d");
   const texture = setupTexture();
-  const ball = setupBall();
+  const balls = setupBalls();
 
   canvas.width = width;
   canvas.height = height;
@@ -110,7 +122,7 @@ const setup = () => {
   state = {
     canvas,
     ctx,
-    ball,
+    balls,
     texture
   };
 };
